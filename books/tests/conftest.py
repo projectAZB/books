c1ebc4e37741d7668d1f8ce03f8011e246893819
Adam Bollard <adambollard@gmail.com>
2019-11-25 12:01:31 -0500

    fix to avoid creating engine each time

--- books/tests/conftest.py
+++ books/tests/conftest.py
@@ -2,15 +2,20 @@ import pytest
 from books.db_engine import DbEngine
 
 
-@pytest.fixture(scope='function')
-def engine():
+@pytest.fixture(scope='session')
+def db_engine():
     db_engine = DbEngine()
-    db_engine.create_all()
     yield db_engine
-    db_engine.drop_all()
     db_engine.db_engine.dispose()
 
 
 @pytest.fixture(scope='function')
-def session_maker(engine):
-    yield engine.session_maker
+def database(db_engine):
+    db_engine.create_all()
+    yield db_engine
+    db_engine.drop_all()
+
+
+@pytest.fixture(scope='function')
+def session_maker(database):
+    yield database.session_maker
