3cee1c41a173ba76a6a7b1cbf6a1ae2c39af09f3
Adam Bollard <adambollard@gmail.com>
2020-02-07 14:55:54 -0500

    use a test db

--- books/settings/__init__.py
+++ books/settings/__init__.py
@@ -7,7 +7,9 @@ environ = int(os.getenv('ENVIRONMENT', 1))  # Default to Development (1)
 host = os.getenv('MYSQL_BOOKS_HOST', '127.0.0.1')
 port = int(os.getenv('MYSQL_BOOKS_PORT', '3306'))
 dbname = os.getenv('MYSQL_BOOKS_DBNAME', 'books')
+test_dbname = os.getenv('MYSQL_BOOKS_TEST_DBNAME', 'books-test')
 user = os.getenv('MYSQL_BOOKS_USER', 'root')
 password = os.getenv('MYSQL_BOOKS_PASSWORD', 'password')
 
 db_url = f'mysql://{user}:{password}@{host}:{port}/{dbname}'
+test_db_url = f'mysal://{user}:{password}@{host}:{port}/{dbname}'
